c9e142314a1f00cede52091333e4cadecfab3ed4
KseniaGon <ksenia_gon@yahoo.com>
2015-10-24 09:17:38 -0700

    Correct warning

--- src/com/example/framework/DriverManager.java
+++ src/com/example/framework/DriverManager.java
@@ -6,7 +6,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
